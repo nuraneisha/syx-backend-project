d6d09bce47ebf3aed34d6d1a47ee69e15bb0e13b
Aneisha Kadir <nuraneisha9@gmail.com>
2025-07-26 13:22:10 +0800

    Update xommand

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ app.post("/users", async (req, res) => {
 app.get("/users/:user_id", async (req, res) => {
     const { user_id } = req.params
     try {
-        const result = await pool.query("SELECT users.*,purchase_history.*,product.prod_category FROM users LEFT JOIN purchase_history ON users.user_id = purchase_history.user_id JOIN product ON purchase_history.prod_id = product.prod_id WHERE users.user_id = $1", [user_id]);
+        const result = await pool.query("SELECT users.*,purchase_history.*,product.prod_category FROM users LEFT JOIN purchase_history ON users.user_id = purchase_history.user_id LEFT JOIN product ON purchase_history.prod_id = product.prod_id WHERE users.user_id = $1", [user_id]);
         console.log(result);
         return res.status(200).json(result.rows);
     } catch (err) {
